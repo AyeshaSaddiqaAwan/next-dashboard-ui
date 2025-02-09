5442e96b631248e4f9508d06cec3108b79db6c22
Ayesha <ayesha.sadd.awan@gmail.com>
2025-02-09 12:02:11 +0500

    updated changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns:[
-            {hostname:"images.pexels.com"}
-        ],
+      remotePatterns: [{ hostname: "images.pexels.com" }],
     },
-};
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
